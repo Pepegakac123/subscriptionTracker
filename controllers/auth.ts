a05a1b87b9277c89632b43401ea630ce882f929f
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-03-11 14:41:31 +0100

    Added createUser auth controller

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,10 +1,66 @@
 import type { NextFunction, Request, Response } from "express";
+import mongoose from "mongoose";
+import User from "../db/models/user.model";
+import { BadRequestError } from "../errors/";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { StatusCodes } from "http-status-codes";
 
 export const signUp = async (
 	req: Request,
 	res: Response,
 	next: NextFunction,
-) => {};
+) => {
+	const session = await mongoose.startSession();
+	session.startTransaction();
+	try {
+		const { name, email, password } = req.body;
+
+		const existingUser = await User.findOne({ email });
+
+		if (existingUser) {
+			throw new BadRequestError("User already exists");
+		}
+
+		//Hashing
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const newUser = await User.create(
+			[{ name, email, password: hashedPassword }],
+			{ session },
+		);
+
+		const JWT_SECRET = process.env.JWT_SECRET as string; // Upewniamy się, że to string
+		const JWT_EXPIRES = process.env.JWT_EXPIRES_IN
+			? Number(process.env.JWT_EXPIRES_IN)
+			: "1d";
+
+		if (!JWT_SECRET) {
+			throw new Error("JWT_SECRET is not defined in environment variables");
+		}
+
+		const token = jwt.sign({ userId: newUser[0].id }, JWT_SECRET, {
+			expiresIn: JWT_EXPIRES, // Sprawdzamy, czy to poprawny format
+		});
+
+		await session.commitTransaction();
+		session.endSession();
+
+		res.status(StatusCodes.CREATED).json({
+			success: true,
+			message: "User created successfully",
+			data: {
+				token,
+				user: newUser[0],
+			},
+		});
+	} catch (error) {
+		await session.abortTransaction();
+		session.endSession();
+		next(error);
+	}
+};
 
 export const signIn = async (
 	req: Request,
